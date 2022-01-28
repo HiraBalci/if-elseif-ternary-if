41c1e3397fb39928009bc2e700353526d6351d14
HiraBalci <93313737+HiraBalci@users.noreply.github.com>
2022-01-28 13:16:34 +0300

    if-elseif-ternary

---
+++ if-elseif-ternary-if/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if_elseif_ternary_if
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            if (time >= 6 && time < 11)
+            {
+                Console.WriteLine("Günaydın!");
+            }
+            else if (time >= 11 && time <= 18)
+            {
+                Console.WriteLine("iyi Günler");
+
+            }
+            else
+            {
+                Console.WriteLine("İyi geceler");
+            }
+                string sonuc = time <= 18 ? "İyi günler" : "İyi geceler";
+            sonuc = time >= 6 && time <= 11 ? "Günaydın" : time >= 11 && time <= 18 ? "İyi günler" : "İyi geceler";
+                Console.WriteLine(sonuc);
+            
+        }
+
+    }
+}
